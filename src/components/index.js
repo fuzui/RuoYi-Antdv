1c43c3a09ba3ca295e91fdccdb6953ecedc9c375
wangze <73400@163.com>
2021-01-14 00:07:10 +0800

    :fire: 去除pro的stable

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,6 @@ import FooterToolbar from '@/components/FooterToolbar'
 import NumberInfo from '@/components/NumberInfo'
 import Tree from '@/components/Tree/Tree'
 import Trend from '@/components/Trend'
-import STable from '@/components/Table'
 import MultiTab from '@/components/MultiTab'
 import IconSelector from '@/components/IconSelector'
 import TagSelect from '@/components/TagSelect'
@@ -23,7 +22,6 @@ export {
   FooterToolbar,
   NumberInfo,
   Tree,
-  STable,
   MultiTab,
   IconSelector,
   TagSelect,
