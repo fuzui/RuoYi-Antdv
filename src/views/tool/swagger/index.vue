78170762d574567f68f9735c4a02c0711e0f4448
fuzui <73400@163.com>
2021-11-22 01:17:18 +0800

    fix: 系统接口url错误

--- src/views/tool/swagger/index.vue
+++ src/views/tool/swagger/index.vue
@@ -14,7 +14,7 @@ export default {
   },
   data () {
     return {
-      src: process.env.VUE_APP_BASE_API + '/swagger-ui.html',
+      src: process.env.VUE_APP_BASE_API + '/swagger-ui/index.html',
       height: document.documentElement.clientHeight - 94.5 + 'px;',
       loading: true
     }
