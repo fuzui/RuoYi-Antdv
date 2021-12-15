167fdaaaf6800fbb95f1dc47ecab7526dcaaf915
fuzui <73400@163.com>
2021-12-16 01:56:54 +0800

    feat: 优化下载解析blob异常提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -145,7 +145,10 @@ export function download (url, params, filename) {
       saveAs(blob, filename)
       message.success('下载成功')
     } else {
-      message.error('无效的会话，或者会话已过期，请重新登录。')
+      const resText = await data.text()
+      const rspObj = JSON.parse(resText)
+      const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode['default']
+      message.error(errMsg)
     }
     notification.close(notificationKey)
   }).catch((r) => {
